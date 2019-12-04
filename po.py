8d897d63c7a0a9fdda0e51023b6c3e9b1712baa3
Kanchanbawariya0306 <54577596+Kanchanbawariya0306@users.noreply.github.com>
2019-12-04 23:08:26 +0530

    Create po.py

---
+++ po.py
@@ -0,0 +1,7 @@
+
+print("\t\tFind the x power of y\n")
+print("1 - x is base number \n 2 - y is the power of x")
+x = float(input("Enter the base number : "))
+y = float(input("Enter the power of x value : "))
+z = x**y
+print("The",x,"power of",y ,"is =",z)
